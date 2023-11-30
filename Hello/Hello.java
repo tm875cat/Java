ff084ff12d0a4f833e79d3dc2e6621383fb59f2c
tm875cat <tm875leter@gmail.com>
2023-11-30 15:50:56 +0800

    test

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -2,6 +2,6 @@ public class Hello {// 類別名稱需要和檔案名稱完全一樣
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("My First Java Program");
-
+        System.out.println("123");
     }
 }
